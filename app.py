998720239ce76ca16c6c71db1ef60e48d18580d8
hanjinglun <1870685625@qq.com>
2024-04-25 05:25:05 -0700

    use flask for test

--- app.py
+++ app.py
@@ -1,35 +1,8 @@
-import cv2
-
-def pull_rtsp_stream(rtsp_url):
-    cap = cv2.VideoCapture(rtsp_url)
-    if not cap.isOpened():
-        print("Error: Unable to open RTSP stream.")
-        return
-    
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("Error: Failed to read frame.")
-            break
-        
-        # 在这里可以对帧进行处理，例如显示到窗口或保存到文件
-        cv2.imshow('RTSP Stream', frame)
-        
-        # 按下 'q' 键退出循环
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    cap.release()
-    cv2.destroyAllWindows()
-
-# # 拉取单个 RTSP 流的示例
-# rtsp_url = "rtsp://192.168.2.198:8554/micagent1"
-# pull_rtsp_stream(rtsp_url)
 
 from flask import Flask, Response
 import cv2
-import sys
-sys.path.append('/home/roota/workstation/onnx2caffe/linux-caffe')
+# import sys
+# sys.path.append('/home/roota/workstation/onnx2caffe/linux-caffe')
 from src import model
 from src import data
 import cv2
@@ -48,22 +21,23 @@ def gen_frames():
 
     while cap.isOpened():
         ret, frame = cap.read()
+        img = data.data_pre_process(frame)
+        output = model.predict(net, img)
+        output = data.data_post_process(output)
+        frame = data.data_paint(output, frame,pose_kind=1)
 
         if not ret:
             break
         else:
             # 将捕获到的帧编码为 JPEG 格式
             ret, buffer = cv2.imencode('.jpg', frame)
-            img = data.data_pre_process(buffer)
-            output = model.predict(net, img)
-            output = data.data_post_process(output)
-            img = data.data_paint(output, buffer)
+            
             if not ret:
                 break
 
             # 将帧作为流传输到网页
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + img.tobytes() + b'\r\n')
+                   b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
 
     cap.release()
 
--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 from src import model
 from src import data
 import cv2
+import time
 
 if __name__ == '__main__':
     model_id = 1001
     device = 0
+    start_time = time.time()
     net=model.load_model(model_id,device)
+    # #photo
+    # img0 = cv2.imread('data/dogandgirl.webp')
+    # img1 = data.data_pre_process(img0)
+    # output = model.predict(net, img1)
+    # output = data.data_post_process(output)
+    # img2=data.data_paint(output,img0)
+    # cv2.imwrite('data/result.jpg', img2)
+    #stream
+    rtsp_url = 'rtsp://192.168.2.198:8554/micagent1'
+    cap = cv2.VideoCapture(rtsp_url)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            img = data.data_pre_process(frame)
+            output = model.predict(net, img)
+            output = data.data_post_process(output)
+            img = data.data_paint(output, frame)
+            cv2.imshow('frame', img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    cap.release()
+    cv2.destroyAllWindows()
 
-    img0 = cv2.imread('/data/dogandgirl.webp')
-    img1 = data.data_pre_process(img0)
+    end_time = time.time()
+    print('runtime:',end_time-start_time)
 
-    output = model.predict(net, img1)
-    
-    output = data.data_post_process(output)
-    img2=data.data_paint(output,img0)
-    cv2.imwrite('data/result.jpg', img2)
