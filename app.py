514a7b91bc3acb982116c1ce851bdde317858913
hanjinglun <1870685625@qq.com>
2024-04-25 23:15:38 -0700

    add index.html

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 
-from flask import Flask, Response
+from flask import Flask, render_template, Response
 import cv2
 # import sys
 # sys.path.append('/home/roota/workstation/onnx2caffe/linux-caffe')
 from src import model
 from src import data
 import cv2
+import base64
 import time
 
 app = Flask(__name__)
 
-def gen_frames():
+def gen_frames(net):
     # 使用 OpenCV 捕获 RTSP 流
     cap = cv2.VideoCapture('rtsp://192.168.2.198:8554/micagent1')
-
-    model_id = 1001
-    device = 0
-    start_time = time.time()
-    net=model.load_model(model_id,device)
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -44,22 +40,16 @@ def gen_frames():
 @app.route('/')
 def index():
     # 返回网页模板，包含视频播放区域
-    return '''
-    <html>
-    <head>
-    <title>RTSP Stream</title>
-    </head>
-    <body>
-    <h1>RTSP Stream</h1>
-    <img src="/video_feed">
-    </body>
-    </html>
-    '''
+    return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
     # 返回生成的帧流
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(net), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    model_id = 1001
+    device = 0
+    start_time = time.time()
+    net=model.load_model(model_id,device)
+    app.run(host='0.0.0.0', port=5000,debug=True)
